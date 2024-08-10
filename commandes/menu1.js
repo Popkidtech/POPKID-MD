413e2e4c515dac5a7ddcf6fed9c19735cd514f9f
Popkidtech <popkidgeee@gmail.com>
2024-08-10 20:19:23 +0300

    Create menu1.js

---
+++ commandes/menu1.js
@@ -0,0 +1,183 @@
+const util = require('util');
+
+const fs = require('fs-extra');
+
+const { zokou } = require(__dirname + "/../framework/zokou");
+
+const { format } = require(__dirname + "/../framework/mesfonctions");
+
+const os = require("os");
+
+const moment = require("moment-timezone");
+
+const s = require(__dirname + "/../set");
+
+
+
+zokou({ nomCom: "ian", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
+
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+
+    let { cm } = require(__dirname + "/../framework//zokou");
+
+    var coms = {};
+
+    var mode = "public";
+
+    
+
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+
+        mode = "private";
+
+    }
+
+
+
+
+
+    
+
+
+
+    cm.map(async (com, index) => {
+
+        if (!coms[com.categorie])
+
+            coms[com.categorie] = [];
+
+        coms[com.categorie].push(com.nomCom);
+
+    });
+
+
+
+    moment.tz.setDefault(s.TZ);
+
+
+
+// Créer une date et une heure en GMT
+
+const temps = moment().format('HH:mm:ss');
+
+const date = moment().format('DD/MM/YYYY');
+
+
+
+  let infoMsg =  `
+
+ Powered by popkid254
+┃   Mode: ${mode}
+┃   User : ${s.OWNER_NAME}
+┃
+ Powered by pokid254
+ Powered by popkid254\n\n`;
+
+
+    
+
+let menuMsg = `
+┏━━━━━━━━━━━━━━┓
+┣❏ian
+┣❏©Taracha📌
+┗━━━━━━━━━━━━━━┛\n
+
+
+
+
+𒈒taracha md cmds𒈒
+`;
+
+
+
+    for (const cat in coms) {
+
+        menuMsg += `┏━━━━━⚼ ${cat}`;
+
+        for (const cmd of coms[cat]) {
+
+            menuMsg += `
+┃📌 ${cmd}`;
+
+        }
+
+        menuMsg += `
+┗━━━━━━━━━━━━━━┛\n`
+
+    }
+
+
+
+    menuMsg += `
+
+
+︎┏━━━━━━━━━━━━━━┓
+️┣❏ian
+┣❏© taracha 
+┗━━━━━━━━━━━━━━┛\n
+
+
+┏━━━━━━━━━━━━━━┓
+┃regards to popkid 
+┗━━━━━━━━━━━━━━┛\n
+
+
+`;
+
+
+
+   var lien = mybotpic();
+
+
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+
+    try {
+
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *JOEL-BOT*, déveloper joel james" , gifPlayback : true }, { quoted: ms });
+
+    }
+
+    catch (e) {
+
+        console.log("🥵🥵 Menu error " + e);
+
+        repondre("🥵🥵 Menu error " + e);
+
+    }
+
+} 
+
+// Vérification pour .jpeg ou .png
+
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+
+    try {
+
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *TKM-bot*, déveloper cod3uchiha" }, { quoted: ms });
+
+    }
+
+    catch (e) {
+
+        console.log("🥵🥵 Menu error " + e);
+
+        repondre("🥵🥵 Menu error " + e);
+
+    }
+
+} 
+
+else {
+
+    
+
+    repondre(infoMsg + menuMsg);
+
+    
+
+}
+
+
+
+});
